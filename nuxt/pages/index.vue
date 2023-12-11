ecf58233e29b026410ce745dcc27824cfcd1836d
Andika <andikaraditya1@gmail.com>
2023-12-11 19:05:46 +0700

    lesson 3

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore sapiente adipisci, pariatur ea a perspiciatis asperiores placeat dolorem incidunt quibusdam, maxime aliquam recusandae. Deleniti praesentium sit natus possimus impedit sapiente?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt/pages/products/hello.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hello Product</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
