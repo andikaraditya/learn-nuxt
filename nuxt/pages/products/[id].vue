6ee72139e582f202a14514cea57f47da2a66645c
Andika <andikaraditya1@gmail.com>
2023-12-11 19:11:03 +0700

    lesson 4 params

--- nuxt/pages/products/[id].vue
+++ nuxt/pages/products/[id].vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <h1>Hello Product</h1>
+        <h2>Id: {{ id }}</h2>
     </div>
 </template>
 
 <script setup>
-
+    const {id} = useRoute().params
 </script>
 
 <style scoped>
